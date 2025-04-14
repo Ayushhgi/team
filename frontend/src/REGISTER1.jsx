a04181c3807a7711479a7a084bfe1ded9ab23a46
tanupawar <tanupawar114@gmail.com>
2025-04-14 14:02:16 +0530

    commit

---
+++ frontend/src/REGISTER1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './REGISTER1.css';
+
+const REGISTER1 = () => {
+  return (
+    <div className="fullscreen">
+      <h1>Welcome to Full Screen View</h1>
+    </div>
+  );
+};
+
+export default REGISTER1;
+
